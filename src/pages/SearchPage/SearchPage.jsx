a5c9cc88e96bd73eecd055d6c4cef676ee765428
Deshawn Brown <brownd726@gmail.com>
2022-05-13 11:53:41 -0500

    Started setup of SearchPage.jsx

---
+++ src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+import { useEffect, useState } from "react/cjs/react.production.min";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth"
+
+const SearchPage = (props) => {
+    return ( pass );
+}
+ 
+export default SearchPage;
