6a9108d206cee6862e29c28352f87b75c359ce94
elijahrosett <rosett3@gmail.com>
2022-05-16 09:21:08 -0700

    Moved axios functon to main app.js folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // General Imports
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import useAuth from "../src/hooks/useAuth";
+
+
+
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import VideoPage from "./pages/VideoPage/VideoPage"
+
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -15,7 +21,36 @@ import Footer from "./components/Footer/Footer";
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
 
+
+
+
+
+
 function App() {
+
+const [user, token] = useAuth();
+const [videos, setVideos] = useState([]);
+const [searchTerm, setSearchTerm] = useState("castles");
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
+  async function fetchVideos(){
+    try {
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=AIzaSyAtdlBhTmpzQ5D-aDGR9PNpfJJAL6aofWY&type=video&relatedToVideo&part=snippet&maxResults=5`);
+      setVideos(response.data.items);
+      console.log(videos)
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+
+
+
+
+
   return (
     <div>
       <Navbar />
@@ -25,7 +60,7 @@ function App() {
           path="/"
           element={
             <PrivateRoute>
-              <HomePage />
+              <HomePage videos ={videos}  />
             </PrivateRoute>
           }
         />
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,34 +4,19 @@ import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
 
-const HomePage = () => {
+const HomePage = (props) => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  const [user, token] = useAuth();
-  const [videos, setVideos] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("castles");
-
-  useEffect(() => {
-    fetchVideos();
-  }, []);
-
-  async function fetchVideos(){
-    try {
-      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=AIzaSyAtdlBhTmpzQ5D-aDGR9PNpfJJAL6aofWY&type=video&relatedToVideo&part=snippet&maxResults=5`);
-      setVideos(response.data.items);
-      console.log(videos)
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
+  
   return (
     <div>
   
-  {videos.map((video) =>{
+  {props.videos.map((video) =>{
     {console.log(video)}
+    
     return <h1> {video.id.videoId}</h1>
+   
         
       })}
   </div>
